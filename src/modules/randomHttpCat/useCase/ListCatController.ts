4e8fd9f11990767057b9fa9be5889cb0b97d065c
Levy Martins Nascimento <levymnascimento@hotmail.com>
2023-01-20 11:35:58 -0300

    Enviando imagem de error na rota de gatos

--- src/modules/randomHttpCat/useCase/ListCatController.ts
+++ src/modules/randomHttpCat/useCase/ListCatController.ts
@@ -1,21 +1,24 @@
 import axios from 'axios';
 import { Request, Response } from 'express';
 
-import { AppError } from '@shared/errors/AppError';
-
 class ListCatController {
   async handle(request: Request, response: Response): Promise<void> {
     try {
       const { code } = request.params;
 
-      const { data } = await axios(`https://http.cat/${code}`, {
+      const { data } = await axios(`https://http.cat/${code || 404}`, {
         responseType: 'arraybuffer',
       });
 
       response.contentType('image/jpg');
       response.end(Buffer.from(data, 'utf-8'));
     } catch {
-      throw new AppError('Gato não encontrado');
+      const { data } = await axios(`https://http.cat/404`, {
+        responseType: 'arraybuffer',
+      });
+
+      response.contentType('image/jpg');
+      response.status(404).end(Buffer.from(data, 'utf-8'));
     }
   }
 }
